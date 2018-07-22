fa944a1499ceae7b84c35312ca9eb7b5d8135149
root <root@localhost.localdomain>
2018-07-22 15:15:42 -0400

    update serve

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -15,6 +15,9 @@ class CORS
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization, X-Requested-With');
     }
 }
